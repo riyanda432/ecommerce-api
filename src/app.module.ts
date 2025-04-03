1e3701dd51a49f21ad4371eb579cf51c63e8b795
Riyanda Febri <riyanda.priyangga@stockbit.com>
2025-04-03 20:46:32 +0700

    remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
-// import { MongooseModule } from '@nestjs/mongoose';
 import { RedisModule } from './common/redis/redis.module';
 import configuration from './config/configuration';
 import { dataSourceOptions } from './database/data-source';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
-// import { CategoriesModule } from './modules/categories/categories.module';
-// import { OrdersModule } from './modules/orders/orders.module';
-// import { CartModule } from './modules/cart/cart.module';
-// import { PromotionsModule } from './modules/promotions/promotions.module';
-// import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
   imports: [
@@ -37,11 +30,6 @@ import { ProductsModule } from './modules/products/products.module';
     UsersModule,
     AuthModule,
     ProductsModule,
-    // CategoriesModule,
-    // OrdersModule,
-    // CartModule,
-    // PromotionsModule,
-    // AdminModule,
   ],
 })
 export class AppModule {}
